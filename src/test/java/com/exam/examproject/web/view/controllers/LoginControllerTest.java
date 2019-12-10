67599d490986f357264f8f38cebe8271489086f0
Emanouil <e_alb|@abv.bg>
2019-12-10 21:07:05 +0200

    Added tests

---
+++ src/test/java/com/exam/examproject/web/view/controllers/LoginControllerTest.java
@@ -0,0 +1,34 @@
+package com.exam.examproject.web.view.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
+public class LoginControllerTest {
+    @BeforeEach
+    private void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Test
+    void getLogin_shouldReturnLoginViewWithResponce200() throws Exception {
+        mockMvc.perform(get("/users/login")).andExpect(status().isOk()).andExpect(view().name("_layouts/index"))
+                .andExpect(model().attribute("view","login"));
+    }
+}
